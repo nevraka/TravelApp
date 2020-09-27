e73bd90260bd133e06c610487c9eb505fd09f8bf
nevraka <nevrakaya@gmail.com>
2020-09-27 23:06:41 +0200

    added App.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HotelList from './components/HotelList';
+import AddNewHotel from './components/AddNewHotel';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/new_hotel">
+          <AddNewHotel></AddNewHotel>
+          <Link to="/">
+            <span>Cancel</span>
+          </Link>
+        </Route>
+        <Route path="/">
+          <HotelList />
+          <Link to="/new_hotel">
+            <span>Otel Ekle</span>
+          </Link>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
