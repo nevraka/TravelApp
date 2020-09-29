403205748d93642a1f7154673045eda972c2b03c
nevraka <nevrakaya@gmail.com>
2020-09-29 22:43:56 +0200

    created update component

---
+++ src/components/hotel/Update.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Form from './Form';
+
+function Update() {
+  const [hotel, setHotel] = useState({});
+  let { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/hotels/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setHotel(result);
+      });
+  }, []);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    fetch(`http://localhost:3000/hotels/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(hotel),
+    }).then(() => history.push('/'));
+  };
+
+  return (
+    <div>
+      <Form
+        buttonText="Update"
+        handleSubmit={handleSubmit}
+        hotel={hotel}
+        setHotel={setHotel}
+      />
+    </div>
+  );
+}
+export default Update;
