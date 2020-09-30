f1efeb9b770f737a971a9e798dcf874f8ffd0af9
nevraka <nevrakaya@gmail.com>
2020-09-30 19:45:52 +0200

    added some route

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import Add from './components/hotel/Add';
 import Update from './components/hotel/Update';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ListCom from './components/comments/ListCom';
+import AddCom from './components/comments/AddCom';
+import UpdateCom from './components/comments/UpdateCom';
 import MainPage from './MainPage';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/hotel/update/:id">
+        <Route path="/hotels/update/:id">
           <Update />
         </Route>
-        <Route path="/hotel/add">
+        <Route path="/hotels/add">
           <Add />
         </Route>
         <Route path="/hotels">
           <List />
-          <Link to="/hotel/add">
+          <Link to="/hotels/add">
             <span>Add Hotel</span>
           </Link>
         </Route>
+        <Route path="/comments/update/:id">
+          <UpdateCom />
+        </Route>
+        <Route path="/comments/add">
+          <AddCom />
+        </Route>
         <Route path="/comments">
           <ListCom />
+          <Link to="/comments/add">
+            <span>Add Comments</span>
+          </Link>
         </Route>
         <Route path="/">
           <MainPage />
