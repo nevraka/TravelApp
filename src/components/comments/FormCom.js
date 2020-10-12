cb4a81c17ac35e9a27b7a5aca4bb7644c9fc7041
nevraka <nevrakaya@gmail.com>
2020-10-12 11:38:16 +0200

    some table changed

--- src/components/comments/FormCom.js
+++ src/components/comments/FormCom.js
@@ -1,54 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input, Layout, Button, Row, Col } from 'antd';
+import Header from '../../Header';
 
-function FormCom({ handleSubmit, comment, setComment, buttonText }) {
+const { Footer, Content } = Layout;
+
+const layout = {
+  labelCol: {
+    span: 3,
+  },
+  wrapperCol: {
+    span: 21,
+  },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 3, span: 21 },
+};
+function FormCom({ handleSubmit, comment, setComment }) {
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Hotel ID
-          <input
-            type="text"
-            value={comment.hotelId}
-            onChange={(e) =>
-              setComment({ ...comment, hotelId: e.target.value })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          User ID
-          <input
-            type="text"
-            value={comment.userId}
-            onChange={(e) => setComment({ ...comment, userId: e.target.value })}
-          />
-        </label>
-        <br />
-        <label>
-          Text
-          <input
-            type="text"
-            value={comment.text}
-            onChange={(e) => setComment({ ...comment, text: e.target.value })}
-          />
-        </label>
-        <br />
-        <label>
-          Date
-          <input
-            type="text"
-            value={comment.dateCreated}
-            onChange={(e) =>
-              setComment({ ...comment, dateCreated: e.target.value })
-            }
-          />
-        </label>
-        <input type="submit" value={buttonText} />
-        <Link to="/">
-          <button>Back</button>
-        </Link>
-      </form>
+      <Header />
+      <Content>
+        <Row>
+          <Col offset={1} span={14}>
+            <Form {...layout} style={{ height: '68vh' }}>
+              <Form.Item label="Hotel ID">
+                <Input
+                  type="text"
+                  value={comment.hotelId}
+                  onChange={(e) =>
+                    setComment({ ...comment, hotelId: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item label="User ID">
+                <Input
+                  type="text"
+                  value={comment.userId}
+                  onChange={(e) =>
+                    setComment({ ...comment, userId: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item label="Text">
+                <Input
+                  type="text"
+                  value={comment.text}
+                  onChange={(e) =>
+                    setComment({ ...comment, text: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item label="Date">
+                <Input
+                  type="text"
+                  value={comment.dateCreated}
+                  onChange={(e) =>
+                    setComment({ ...comment, dateCreated: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item {...tailLayout}>
+                <Button type="primary" onClick={handleSubmit}>
+                  Add
+                </Button>
+                <Link to="/">
+                  <Button>Back</Button>
+                </Link>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Content>
+      <Footer />
     </div>
   );
 }
