9e60a67d06068f2538763cd452a6b13a603a9d40
nevraka <nevrakaya@gmail.com>
2020-10-12 11:37:34 +0200

    changed name form component

---
+++ src/components/hotel/Formh.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input, Layout, Button, Row, Col } from 'antd';
+import Header from '../../Header';
+
+const { Footer, Content } = Layout;
+
+const layout = {
+  labelCol: {
+    span: 3,
+  },
+  wrapperCol: {
+    span: 21,
+  },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 3, span: 21 },
+};
+
+function Formh({ handleSubmit, hotel, setHotel, buttonText }) {
+  return (
+    <div>
+      <Header />
+      <Content>
+        <Row>
+          <Col offset={1} span={14}>
+            <Form {...layout} style={{ height: '68vh' }}>
+              <Form.Item label="Hotel">
+                <Input
+                  type="text"
+                  value={hotel.name}
+                  onChange={(e) => setHotel({ ...hotel, name: e.target.value })}
+                />
+              </Form.Item>
+              <Form.Item label="City">
+                <Input
+                  type="text"
+                  value={hotel.city}
+                  onChange={(e) => setHotel({ ...hotel, city: e.target.value })}
+                />
+              </Form.Item>
+              <Form.Item label="Counrty">
+                <Input
+                  type="text"
+                  value={hotel.country}
+                  onChange={(e) =>
+                    setHotel({ ...hotel, country: e.target.value })
+                  }
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" onClick={handleSubmit}>
+                  Add
+                </Button>
+                <Link to="/">
+                  <Button>Back</Button>
+                </Link>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Content>
+      <Footer />
+    </div>
+  );
+}
+
+export default Formh;
