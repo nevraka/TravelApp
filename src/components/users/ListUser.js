d83fb57aa0bd1f5dd02e62b8a57d4ae4b126c5b6
nevraka <nevrakaya@gmail.com>
2020-10-13 15:11:19 +0200

    render funct. ListUser

--- src/components/users/ListUser.js
+++ src/components/users/ListUser.js
@@ -1,32 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
 import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 function ListUser() {
   const [users, setUsers] = useState([]);
 
+  const renderCell = (text, record) => (
+    <Link to={`/users/update/${record.id}`}>{text}</Link>
+  );
   const columns = [
     {
       title: 'Name',
       dataIndex: 'name',
-      //   render: renderCell,
+      render: renderCell,
     },
     {
       title: 'Address',
       dataIndex: 'address',
-      //   render: renderCell,
+      render: renderCell,
     },
     {
       title: 'ID',
       dataIndex: 'id',
-      //   render: renderCell,
+      render: renderCell,
     },
     {
       title: 'Action',
       key: 'action',
-      //   render: (_, { id }) => (
-      // <Button onClick={() => handleDelete(id)}>Delete</Button>
-      //   ),
+      render: (_, { id }) => (
+        <Button onClick={() => handleDelete(id)}>Delete</Button>
+      ),
     },
   ];
   useEffect(() => {
@@ -36,6 +40,28 @@ function ListUser() {
         setUsers(results);
       });
   }, []);
+
+  const handleDelete = async (id) => {
+    fetch(`http://localhost:3000/users/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          const remaining = users.filter((h) => id !== h.id);
+          setUsers(remaining);
+        } else {
+          throw `status code: ${response.status}`;
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        alert('An error accured');
+      });
+  };
 
   return (
     <Table
